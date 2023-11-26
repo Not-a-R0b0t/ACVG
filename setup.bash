152d223c7dd31a1314b00eb0900016aef7c1ba3c
Not-a-R0b0t <152111009+Not-a-R0b0t@users.noreply.github.com>
2023-11-27 05:23:48 +0530

    Add files via upload

---
+++ setup.bash
@@ -0,0 +1,23 @@
+#This code is developed by the Authors of CVPR submission Paper ID:17749. 
+export IMG_H=128
+export IMG_W=128
+export PAST_TS=5
+export FUTURE_TS=10
+export TEST_FUTURE_TS=20
+export C_DIM=3
+export A_DIM=2
+export BATCH_SZ=8
+export EPOCHS=2500
+export EPOCHS_ACTOR=1000
+export EPOCHS_ACVG=1500
+export GPU_ID=1
+export FILTERS=32
+export A_FILTERS=32
+export G_TRAIN=True
+export A_TRAIN=True
+export CKPT_G=100
+export CKPT_D=100
+export CKPT_A=50
+export TEST_G=False
+# export DATAPATH="../data/RoAM_dataset/processed/train"
+export DATAPATH="../data/RoAM_dataset/processed/test"
